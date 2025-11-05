67a6515d9eb581c2051c0f38ed1ea0195e4e8306
supunShashika <supunshashika.davinda@gmail.com>
2025-11-05 10:55:43 +0530

    N.A.S.S.Devinda
    Change back to correct mobile no

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import string
 import secrets
 from datetime import datetime, timedelta
 from flask import Flask, jsonify, redirect, url_for, session, request, render_template, flash
-from requests_oauthlib import OAuth2Session
+
 import mysql.connector
-from dotenv import load_dotenv
+
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 from werkzeug.security import generate_password_hash, check_password_hash
